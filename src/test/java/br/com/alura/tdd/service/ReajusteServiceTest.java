5386ec11d72709b16e4b58870ade3c175294685a
Vogon38 <luana@thecyborg.xyz>
2021-12-05 17:50:40 -0300

    Adicionado testes unitarios para todos os desempenhos

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -21,4 +21,27 @@ public class ReajusteServiceTest {
         assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 
     }
+
+    @Test
+    public void reajusteDeveriaSerQuinzePorCentoDesempenhoBom(){
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
+
+        service.concederReajuste(funcionario, Desempenho.BOM);
+
+        assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
+
+    }
+
+    @Test
+    public void reajusteDeveriaSerVintePorCentoDesempenhoOtimo(){
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
+
+        service.concederReajuste(funcionario, Desempenho.OTIMO);
+
+        assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
+
+    }
+
 }
