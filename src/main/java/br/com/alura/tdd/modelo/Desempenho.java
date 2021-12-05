7a19bfd9992fbfe78f2dce880edfdbaf95dd6ccc
Vogon38 <luana@thecyborg.xyz>
2021-12-05 18:25:00 -0300

    Refatoração do Enum e ReajusteService

--- src/main/java/br/com/alura/tdd/modelo/Desempenho.java
+++ src/main/java/br/com/alura/tdd/modelo/Desempenho.java
@@ -1,7 +1,24 @@
 package br.com.alura.tdd.modelo;
 
+import java.math.BigDecimal;
+
 public enum Desempenho {
-   A_DESEJAR,
-   BOM,
-   OTIMO;
+   A_DESEJAR {
+      public BigDecimal percentualReajuste(){
+         return new BigDecimal("0.03");
+      }
+   },
+   BOM {
+      public BigDecimal percentualReajuste(){
+         return new BigDecimal("0.15");
+      }
+   },
+   OTIMO {
+      public BigDecimal percentualReajuste(){
+         return new BigDecimal("0.20");
+      }
+   };
+
+   public abstract BigDecimal percentualReajuste();
+
 }
